89e2701405c4165efce0012f1ea9cc1c59443208
Thị Kim Cúc <cucntk1@onemount.com>
2021-09-06 18:42:19 +0700

    update

--- Bai4/Bai4.py
+++ Bai4/Bai4.py
@@ -57,3 +57,19 @@ for i in range(n):
     my_list.append(int(input("Nhập từng phần tử trong list: ")))
 print("List đã nhập là: ",my_list)
 print (Counter(my_list) )
+## Bài tập Dictionary Key
+my_list = []
+my_dict1=dict(Trang=38, Thu=38, Ngoc=27, Thanh=26, Yen=25, Hang=22, Thuy=22)
+my_dict2= [{"V":"S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII":"S005"}, {"V":"S009"},{"VIII":"S007"}]
+for i in my_dict1.values(): 
+  if i in my_list: 
+    continue 
+  else:
+    my_list.append(i)
+for j in range(0, len(my_dict2)):
+  c = list(my_dict2[j].values())
+  if c in my_list: 
+    continue 
+  else:
+    my_list.append(c)
+print(my_list)
